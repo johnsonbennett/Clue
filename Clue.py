c8f59695e503f4ceee1bd8ade249926426fcc7b2
johnsonbennett <82695431+johnsonbennett@users.noreply.github.com>
2022-06-02 12:19:35 -0500

    Update Clue.py
    Just finished off the starting page for the game.  The GUI should ask for the total number of players and then display the players deck based on player number. Did not do testing conditions

--- Clue.py
+++ Clue.py
@@ -32,46 +32,58 @@ total_list = suspect_list + weapon_list + room_list # Combining all the catagori
 random.shuffle(total_list)#Shuffling the list which is used for the game
 #print(total_list)
 
-def player_choice():
-    condition=True
-    while(condition==True):
-        num_players = int(input("Enter no of players (2-7): "))
-        if(num_players > 7 or num_players<2):                   #Check statement to make sure that the user enter the correct input
-            print("Invalid Selection, try again!")
-        else:
-            condition=False
-    return num_players
-players = player_choice()
+#def player_choice():
+ #   condition=True
+  #  while(condition==True):
+   #     num_players = int(input("Enter no of players (2-7): "))
+    #    if(num_players > 7 or num_players<2):                   #Check statement to make sure that the user enter the correct input
+     #       print("Invalid Selection, try again!")
+      ##     condition=False
+    #return num_players
+#players = player_choice()
 
 #This function is used to create a deck for the players
-def deck_creation(players):
+def deck_creation():
+    player_choice = int(startup_players.get())
+    playerNo = int(player_num.get())
     deck = {} 
-    for i in range(players):
+    for i in range(player_choice):
          deck[i] = []                                       #Creating a dictionary were we can store each player list as elements
     counter = 0
     i=0
     while(counter < len(total_list)):
-        if(i>=players):
+        if(i>=player_choice):
             i=0
         deck[i].append(total_list[counter])
         counter=counter+1
         i=i+1
-    print(deck)
-        
-    return deck
+    player_deck = deck[playerNo-1]
+    display_deck.insert(0,f'{player_deck}')
+    
+    return deck[playerNo-1]
 
-final_deck_list = deck_creation(players)
+#final_deck_list = deck_creation(players)
 
+#This is the start up GUI for the game
 clue = tk.Tk()
 clue.title("Clue Start up Page")
-startup_message = tk.Label(clue, text="Welcome to Clues!!")
-theme=ttk.Label(clue, text = "Themed label")
-theme.pack()
-startup_message.pack()
+startup_message = ttk.Label(clue, text="Welcome to Clues!!").pack()
+
 clue.geometry('600x400+50+50')
-startup_players = ttk.Button(clue,text="Choose no of players",command = players)
-startup_deck = ttk.Button(clue,text="Show starting deck",command = final_deck_list)
+player_label = ttk.Label(clue,text = "Enter number of players").pack()
+startup_players = ttk.Entry(clue,width = 35)
+startup_players.pack()
+player_num_label = ttk.Label(clue, text = "Enter player number").pack()
+player_num = ttk.Entry(clue,width =35)
+player_num.pack()
+startup_deck = ttk.Button(clue,text="Show starting deck",command = deck_creation)
+display_deck = ttk.Entry(clue,width = 42)
 exit = ttk.Button(clue, text ="Exit",command = lambda:clue.quit())
+startup_players.pack()
+startup_deck.pack()
+display_deck.pack()
+display_label = ttk.Label(clue, text = "Your deck will show up in the space above").pack()
+exit.pack(side = tk.BOTTOM)
 clue.mainloop()
 
 
