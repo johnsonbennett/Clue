5542955792c4a99d9f0f924417c07bd5ed8d02a4
johnsonbennett <82695431+johnsonbennett@users.noreply.github.com>
2022-06-02 13:53:58 -0500

    Update Clue.py
    Added another window into the mix were the actual gameplay occurs. No more data is shown in the console, everything is shown in GUI

--- Clue.py
+++ Clue.py
@@ -30,22 +30,12 @@ gameGoal(choice_list,suspect_list,weapon_list,room_list)
 
 total_list = suspect_list + weapon_list + room_list # Combining all the catagories into one list
 random.shuffle(total_list)#Shuffling the list which is used for the game
-#print(total_list)
-
-#def player_choice():
- #   condition=True
-  #  while(condition==True):
-   #     num_players = int(input("Enter no of players (2-7): "))
-    #    if(num_players > 7 or num_players<2):                   #Check statement to make sure that the user enter the correct input
-     #       print("Invalid Selection, try again!")
-      ##     condition=False
-    #return num_players
-#players = player_choice()
 
 #This function is used to create a deck for the players
 def deck_creation():
     player_choice = int(startup_players.get())
     playerNo = int(player_num.get())
+    
     deck = {} 
     for i in range(player_choice):
          deck[i] = []                                       #Creating a dictionary were we can store each player list as elements
@@ -59,33 +49,42 @@ def deck_creation():
         i=i+1
     player_deck = deck[playerNo-1]
     display_deck.insert(0,f'{player_deck}')
-    
-    return deck[playerNo-1]
 
-#final_deck_list = deck_creation(players)
 
 #This is the start up GUI for the game
-clue = tk.Tk()
-clue.title("Clue Start up Page")
-startup_message = ttk.Label(clue, text="Welcome to Clues!!").pack()
+clue_start = tk.Tk()
+clue_start.title("Clue Start up Page")
+startup_message = ttk.Label(clue_start, text="Welcome to Clues!!",font=("Ariel",25)).pack(pady=15)
 
-clue.geometry('600x400+50+50')
-player_label = ttk.Label(clue,text = "Enter number of players").pack()
-startup_players = ttk.Entry(clue,width = 35)
+clue_start.geometry('600x500+60+50')
+player_label = ttk.Label(clue_start,text = "Enter number of players").pack(pady = 10)
+startup_players = ttk.Entry(clue_start,width = 35)
 startup_players.pack()
-player_num_label = ttk.Label(clue, text = "Enter player number").pack()
-player_num = ttk.Entry(clue,width =35)
-player_num.pack()
-startup_deck = ttk.Button(clue,text="Show starting deck",command = deck_creation)
-display_deck = ttk.Entry(clue,width = 42)
-exit = ttk.Button(clue, text ="Exit",command = lambda:clue.quit())
-startup_players.pack()
-startup_deck.pack()
-display_deck.pack()
-display_label = ttk.Label(clue, text = "Your deck will show up in the space above").pack()
+player_num_label = ttk.Label(clue_start, text = "Enter player number").pack(pady=10)
+player_num = ttk.Entry(clue_start,width =35)
+player_num.pack(pady=5)
+startup_deck = ttk.Button(clue_start,text="Show starting deck",command = deck_creation)
+display_deck = ttk.Entry(clue_start,width = 42)
+exit = ttk.Button(clue_start, text ="Exit",command = lambda:clue_start.quit())
+startup_deck.pack(pady=10)
+display_deck.pack(pady=10)
+display_label = ttk.Label(clue_start, text = "Your deck will show up in the space above").pack()
+
+
+#The following code is for the game play window and so should be done on a different window
+#I am creating another frame were the game play occurs
+def gamePlay():
+    clue_game = tk.Tk()
+    clue_game.title("Clue")
+    clue_game.geometry('400x400+50+50')
+    exit = ttk.Button(clue_game, text = "Exit",command = lambda:clue_game.quit())
+    exit.pack(side = tk.BOTTOM)
+    clue_game.mainloop()
+start_game = tk.Button(clue_start,text = "PLAY",command=gamePlay,height =3,width= 10).pack(pady=10)
 exit.pack(side = tk.BOTTOM)
-clue.mainloop()
 
+
+clue_start.mainloop()
 
 
  
