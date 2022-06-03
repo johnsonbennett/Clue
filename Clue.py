fc84630fea91967ce5dc52b0c3e12e5b4a39a859
johnsonbennett <82695431+johnsonbennett@users.noreply.github.com>
2022-06-02 19:56:46 -0500

    Update Clue.py
    Creating the board map for the game in the second GUI frame

--- Clue.py
+++ Clue.py
@@ -3,6 +3,7 @@
 import random
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 
 
 suspect_list = ["Mrs Peacock","Rev Green","Colonel Mustard","Prof Plum","Miss Scarlet","Nurse White"]
@@ -40,28 +41,30 @@ def deck_creation():
     for i in range(player_choice):
          deck[i] = []                                       #Creating a dictionary were we can store each player list as elements
     counter = 0
-    i=0
+    i=0                                                     #using the for and while loop to distribute the cards to all the players
     while(counter < len(total_list)):
         if(i>=player_choice):
             i=0
         deck[i].append(total_list[counter])
         counter=counter+1
         i=i+1
-    player_deck = deck[playerNo-1]
+    player_deck = deck[playerNo-1]                          #used to display the deck for the specific player
     display_deck.insert(0,f'{player_deck}')
 
 
 #This is the start up GUI for the game
 clue_start = tk.Tk()
 clue_start.title("Clue Start up Page")
-startup_message = ttk.Label(clue_start, text="Welcome to Clues!!",font=("Ariel",25)).pack(pady=15)
+img = PhotoImage(file = "C:/Users/johns/OneDrive/Pictures/Saved Pictures/clue-logo-7.png")     #Attached an image which is used in the welcome screen 
+ttk.Label(clue_start,image = img).pack()
+startup_message = ttk.Label(clue_start, text="Welcome to Clue!!",font=("Ariel",25)).pack(pady=15)
 
-clue_start.geometry('600x500+60+50')
+clue_start.geometry('600x550+60+50')
 player_label = ttk.Label(clue_start,text = "Enter number of players").pack(pady = 10)
-startup_players = ttk.Entry(clue_start,width = 35)
+startup_players = ttk.Entry(clue_start,width = 10)
 startup_players.pack()
 player_num_label = ttk.Label(clue_start, text = "Enter player number").pack(pady=10)
-player_num = ttk.Entry(clue_start,width =35)
+player_num = ttk.Entry(clue_start,width =10)
 player_num.pack(pady=5)
 startup_deck = ttk.Button(clue_start,text="Show starting deck",command = deck_creation)
 display_deck = ttk.Entry(clue_start,width = 42)
@@ -76,16 +79,42 @@ display_label = ttk.Label(clue_start, text = "Your deck will show up in the spac
 def gamePlay():
     clue_game = tk.Tk()
     clue_game.title("Clue")
-    clue_game.geometry('400x400+50+50')
-    exit = ttk.Button(clue_game, text = "Exit",command = lambda:clue_game.quit())
-    exit.pack(side = tk.BOTTOM)
-    clue_game.mainloop()
+    clue_game.geometry('600x600+50+50')
+    clue_game.resizable(False,False)
+    clue_game.columnconfigure(0,weight=2)
+    clue_game.columnconfigure(2,weight=2)
+    clue_game.columnconfigure(4,weight=2)
+    clue_game.rowconfigure(0,weight=1)
+    clue_game.rowconfigure(1,weight=1)
+    clue_game.rowconfigure(2,weight=1)
+    clue_game.rowconfigure(3,weight=1)
+    clue_game.rowconfigure(4,weight=1)
+    room1_label = tk.Label(clue_game, text = "Ballroom",font=("Ariel",14),bg="pink",borderwidth=3,relief="groove",height=5,width=20)
+    room1_label.grid(column=0,row=0,sticky=tk.W,padx=10,pady=15)
+    room2_label = tk.Label(clue_game,text = "Main Hall", font=("Ariel",14),bg="orange",borderwidth=3,relief="groove",height=5,width=20)
+    room2_label.grid(column=0,row=2,sticky=tk.NW,padx=10,pady=3)
+    room3_label = tk.Label(clue_game,text = "Lounge",font=("Ariel", 14),bg="green",borderwidth=3,relief="groove",height=5,width=20)
+    room3_label.grid(column=0,row=3,sticky=tk.W,padx=10,pady=13)
+    room4_label = tk.Label(clue_game,text ="Kitchen",font=("Ariel",14),bg="violet",borderwidth=3,relief="groove",height=5,width=20)
+    room4_label.grid(column=4,row=0,sticky=tk.E,padx=15,pady=13)
+    exit_game = ttk.Button(clue_game, text = "Exit",command = lambda:clue_game.quit())
+    
+
+
+
+
+
+
+
+
+
+
+
 start_game = tk.Button(clue_start,text = "PLAY",command=gamePlay,height =3,width= 10).pack(pady=10)
 exit.pack(side = tk.BOTTOM)
 
 
 clue_start.mainloop()
-
 
  
 
