76975072c0697772c87cc3e487ed83bed14a8fe0
johnsonbennett <82695431+johnsonbennett@users.noreply.github.com>
2022-06-07 15:36:34 -0500

    Update Clue.py

--- Clue.py
+++ Clue.py
@@ -30,7 +30,6 @@ def gameGoal(main_list,list1,list2,list3):
 gameGoal(choice_list,suspect_list,weapon_list,room_list)
 
 total_list = suspect_list + weapon_list + room_list # Combining all the catagories into one list
-random.shuffle(total_list)#Shuffling the list which is used for the game
 
 clue_start=tk.Tk()
 clue_start.geometry('600x550')
@@ -43,6 +42,7 @@ clue_start.resizable(False,False)
 
 def gamePlay():
     counter=6
+   
     clue_game = tk.Tk()
     clue_game.title("Clue")
     clue_game.configure(bg="lightgrey")
@@ -51,13 +51,13 @@ def gamePlay():
     random_deck=[]                                                                  #Choosing this empty list to store the initial clue list
     random_weapon=random.choice(weapon_list)
     random_deck.append(random_weapon)
-    total_list.remove(random_weapon)                            
+    weapon_list.remove(random_weapon)                            
     random_room=random.choice(room_list)                                             #These set of statements choose 1 card from each catagory as clue
     random_deck.append(random_room)
-    total_list.remove(random_room)
+    room_list.remove(random_room)
     random_suspect=random.choice(suspect_list)
     random_deck.append(random_suspect)
-    total_list.remove(random_suspect)
+    suspect_list.remove(random_suspect)
     
     #Configuring board grid layout
     clue_game.columnconfigure(0,weight=2)
@@ -69,82 +69,115 @@ def gamePlay():
     clue_game.rowconfigure(4,weight=1)
 
     #This function delivers clues for the player by sacrificing a chance
-    def clue_delivery(counter):
-        random_clue=random.choice(total_list)
-        answer=messagebox.askyesno(title='Validation',message='Are you sure, one of your chance will be deducted')
-        if answer:
-             counter=counter-1
-             messagebox.showinfo(title='Clue',message=random_clue)
+    def clue_delivery(counter,total_list):
+          random_clue=random.choice(total_list)
+          answer=messagebox.askyesno(title='Validation',message='Are you sure, one of your chance will be deducted')
+          if answer:
+               counter=counter-1
+               messagebox.showinfo(title='Clue',message=random_clue)
+          return counter
 
-    def display_counter(counter):
-         if(counter<2):
-              messagebox.showwarning(title='Warning',message='Reaching limit you have % chances ' %(counter))
-         else:
-              messagebox.showinfo(title='Chance',message='You have % chances left' %(counter))
+    def display_counter():
+          if(counter<2):
+               messagebox.showwarning(title='Warning',message='Reaching limit you have % chances ' %(counter))
+          else:
+               messagebox.showinfo(title='Chance',message='No of chances left: ' f'{counter}')
+
+    def random_cat():
+        cat=['room','suspect','weapon']
+        random_cat=random.choice(cat)
+        if(random_cat=='room'):
+            messagebox.showinfo(title='Draw Clue',message=random.choice(room_list))
+        if(random_cat=='suspect'):
+            messagebox.showinfo(title='suspect',message=random.choice(suspect_list))
+        if(random_cat=='weapon'):
+            messagebox.showinfo(title='weapon',message=random.choice(weapon_list))
 
 
     
         
     #Setting room box in the game board
     #Clicking room buttons should reveal a random clue for the player and the counter should go down by 1
-    room1_label = tk.Button(clue_game, text = "Ballroom",font=("Ariel",14),bg="pink",command=lambda:clue_delivery(counter),borderwidth=3,relief="groove",height=5,width=20,state="normal")
-    room1_label.grid(column=0,row=0,sticky=tk.W,padx=10,pady=13)
-    room2_label = tk.Button(clue_game,text = "Main Hall", font=("Ariel",14),bg="orange",borderwidth=3,command=lambda:clue_delivery(counter),relief="groove",height=5,width=20,state="normal")
-    room2_label.grid(column=0,row=1,sticky=tk.NW,padx=10,pady=13)
-    room3_label = tk.Button(clue_game,text = "Lounge",font=("Ariel", 14),bg="green",borderwidth=3,command=lambda:clue_delivery(counter),relief="groove",height=5,width=20,state="normal",fg="white")
-    room3_label.grid(column=0,row=2,sticky=tk.NW,padx=10,pady=13)
-    room4_label = tk.Button(clue_game,text ="Kitchen",font=("Ariel",14),bg="violet",borderwidth=3,command=lambda:clue_delivery(counter),relief="groove",height=5,width=20,state="normal")
-    room4_label.grid(column=4,row=0,sticky=tk.E,padx=15,pady=13)
-    room5_label = tk.Button(clue_game, text ="Bedroom",font=("Ariel",14),bg="indigo",borderwidth=3,command=lambda:clue_delivery(counter),relief="groove",height=5,width=20,state="normal")
-    room5_label.grid(column=4,row=1,sticky=tk.NE,padx=10,pady=13)
-    room6_label = tk.Button(clue_game,text="Living Room",font=("Ariel",14),bg="lightblue",borderwidth=3,command=lambda:clue_delivery(counter),relief="groove",height=5,width=20,state="normal")
-    room6_label.grid(column=4,row=2,sticky=tk.NE,padx=10,pady=13)
-    mainClue_label = tk.Label(clue_game,text="Clue",font=("Ariel",16),bg="white",borderwidth=3,relief="groove",height=8,width=15)
-    mainClue_label.grid(column=1,row=1,padx=10,pady=13)
+    room = tk.Button(clue_game, text = "Ballroom",font=("Ariel",14),bg="pink",borderwidth=3,relief="groove",height=5,width=20,state=tk.DISABLED)
+    room.grid(column=0,row=0,sticky=tk.W,padx=10,pady=13)
+    room = tk.Button(clue_game,text = "Main Hall", font=("Ariel",14),bg="orange",borderwidth=3,relief="groove",height=5,width=20,state= tk.DISABLED)
+    room.grid(column=0,row=1,sticky=tk.NW,padx=10,pady=13)
+    room = tk.Button(clue_game,text = "Lounge",font=("Ariel", 14),bg="green",borderwidth=3,relief="groove",height=5,width=20,state=tk.DISABLED,fg="white")
+    room.grid(column=0,row=2,sticky=tk.NW,padx=10,pady=13)
+    room = tk.Button(clue_game,text ="Kitchen",font=("Ariel",14),bg="violet",borderwidth=3,relief="groove",height=5,width=20,state= tk.DISABLED)
+    room.grid(column=4,row=0,sticky=tk.E,padx=15,pady=13)
+    room = tk.Button(clue_game, text ="Bedroom",font=("Ariel",14),bg="indigo",borderwidth=3,relief="groove",height=5,width=20,state= tk.DISABLED)
+    room.grid(column=4,row=1,sticky=tk.NE,padx=10,pady=13)
+    room = tk.Button(clue_game,text="Living Room",font=("Ariel",14),bg="lightblue",borderwidth=3,relief="groove",height=5,width=20,state= tk.DISABLED)
+    room.grid(column=4,row=2,sticky=tk.NE,padx=10,pady=13)
+    clue_label = tk.Label(clue_game,text="Clue",font=("Ariel",16),bg="white",borderwidth=3,relief="groove",height=8,width=15)
+    clue_label.grid(column=1,row=1,padx=10,pady=13)
     starting_deck=tk.Label(clue_game,text=f'{random_deck}',bg="lightgreen",borderwidth=3,relief="groove",height=3,width=50).grid(column=1,row=3)
-    clue_choice=Button(clue_game,text='Click to receive a clue',command=lambda:clue_delivery(counter),width=30,borderwidth=3,relief='groove').grid(column=2,row=1)
-    display_counter=Button(clue_game,text='Show no of chances',command=lambda:display_counter(counter)).grid(column=2,row=2,sticky=tk.W)
+    clue_choice=Button(clue_game,text='Click to receive a clue',command=lambda:clue_delivery(counter,total_list),width=30,borderwidth=3,relief='groove').grid(column=2,row=1)
+    display_counter=Button(clue_game,text='Show no of chances',command=display_counter).grid(column=2,row=2,sticky=tk.W)
 
+    count=3
+    def draw_count(count):
+        while(count!=0):
+            item=tk.StringVar()
+            draw_clue= Button(clue_game,text='Draw',textvariable=item,command=random_cat,state=tk.ACTIVE).grid(column=2,row=1,sticky=tk.S)
+            count=count-1
+        if(draw_clue['state']==tk.ACTIVE):
+            draw_clue['state']==tk.DISABLED
 
+   # draw_count(count)
     final_suspect=Label(clue_game,text="Suspect Selection: ").grid(column=1,row=0,sticky=tk.W)
     final_weapon=Label(clue_game,text="Weapon Selection: ").grid(column=1,row=0)
     final_room=Label(clue_game,text="Room selection: ").grid(column=1,row=0,sticky=tk.E)
-    suspect_entry=tk.Entry(clue_game).grid(column=1,row=0,sticky=tk.SW)
-    weapon_entry=tk.Entry(clue_game).grid(column=1,row=0,sticky=tk.S)
-    room_entry=tk.Entry(clue_game).grid(column=1,row=0,sticky=tk.SE)
-
-        #This is the function that checks whether the player selection is the correct combination
+    
+    item_1 = tk.StringVar()
+    item_2=tk.StringVar()
+    item_3=tk.StringVar()
+    suspect_entry=tk.Entry(clue_game,textvariable=item_1).grid(column=1,row=0,sticky=tk.SW)
+    weapon_entry=tk.Entry(clue_game,textvariable=item_2).grid(column=1,row=0,sticky=tk.S)
+    room_entry=tk.Entry(clue_game,textvariable=item_3).grid(column=1,row=0,sticky=tk.SE)
+    print(choice_list)
+    entry_1=item_1.get()            
+    entry_2=item_2.get()
+    entry_3=item_3.get()
+    
+    #This is the function that checks whether the player selection is the correct combination
     def searchAlgorithm(choice_list,counter):
-              entry_1=suspect_entry.get()            
-              entry_2=weapon_entry.get()
-              entry_3=room_entry.get()
-              correct=0
-              for i in range(3):
-                 if(choice_list[i]==entry_1):
-                      correct=correct+1               #Using the search algorithm were each index of the list is checked with the target variable
-                 if(choice_list[i]==entry_2):
-                      correct=correct+1
-                 if(choice_list[i]==entry_3):
-                      correct=correct+1
-                 if(correct==3):
-                    messagebox.showinfo(title='Winner',message='Congragulation,you figured it out!!!')
-                    counter=counter-1
-                 if(correct==2):
-                    messagebox.showwarning(title='Feedback',message="Almost there, 1 wrong selection. Try again")
-                    counter=counter-1
-                 if(correct==1):
-                    messagebox.showwarning(title='Feedback',message="2 wrong selection.Try again")
-                    counter=counter-1
-                 if(correct==0):
-                    messagebox.showwarning(title='Feedback',message="Wrong answer Try again")
-                    counter=counter-1                                                           
-        
+                  
+                  Label(clue_game,text=entry_1).grid(column=1,row=2)
+                  print(entry_1)
+                  print(entry_2)
+                  print(entry_3)
+                  correct=0
+                  for i in range(3):
+                     print(choice_list[i])
+                     print(entry_1)
+                     if(choice_list[i]== entry_1):
+                         correct=correct-1               #Using the search algorithm were each index of the list is checked with the target variable
+                     if(choice_list[i]== entry_2):
+                         correct=correct-1
+                     if(choice_list[i]== entry_3):
+                         correct=correct-1
+                  if(correct==3):
+                        messagebox.showinfo(title='Winner',message='Congragulation,you figured it out!!!')
+                        clue_game.destroy()
 
+                  if(correct==2):
+                        messagebox.showwarning(title='Feedback',message="Almost there, 1 wrong selection. Try again")
+                        
+                  if(correct==1):
+                        messagebox.showwarning(title='Feedback',message="2 wrong selection.Try again")
+                        
+                  if(correct==0):
+                        messagebox.showwarning(title='Feedback',message="Wrong answer Try again")
+                  counter=counter-1 
+                  return counter
+        
     check_selection=Button(clue_game,text="Check Answer",command=lambda:searchAlgorithm(choice_list,counter),borderwidth=3,relief="groove",bg="pink").grid(column=1,row=2,sticky=tk.N)
   
 
 
-start_game = tk.Button(clue_start,text = "PLAY",command=gamePlay,height =5,width= 20,borderwidth=3,relief='groove').pack(pady=10)
+start_game = tk.Button(clue_start,text = "PLAY",command=gamePlay,height =5,width= 20,borderwidth=3,relief='groove').pack(pady=30)
 
 clue_start.mainloop()
 
